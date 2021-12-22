4c46d72c825daad576c69cb17d2bde54a9f2852a
zhanghao <308507814@qq.com>
2021-12-22 17:23:25 +0800

    change vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict"
 const path = require('path')
 function resolve(dir) {
@@ -25,32 +24,4 @@ module.exports = {
             }
         }
     },
-=======
-"use strict"
-const path = require('path')
-function resolve(dir) {
-    return path.join(__dirname, dir)
-}
-module.exports = {
-    publicPath: '/',
-    configureWebpack: {
-        resolve: {
-            alias: {
-                "@": resolve("src")
-            }
-        }
-    },
-    css: {
-        loaderOptions: {
-            css: {},
-            postcss: {
-                plugins: [
-                    require('postcss-px2rem')({
-                        remUnit: 192 //设计图宽度大小/10之后的数值
-                    })
-                ]
-            }
-        }
-    },
->>>>>>> 2874230260ea3bfd314133c1952c5de60b3cdb44
 }
